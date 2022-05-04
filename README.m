4c873b81a8652dd5514edef2dbd2ec14901d78c3
Mohanraj3521 <102143984+Mohanraj3521@users.noreply.github.com>
2022-05-04 11:37:18 +0530

    Update README.m

--- README.m
+++ README.m
@@ -1,8 +1,8 @@
-Pix-OTT Platform
+Pix-ott Platform
 
 ## Project Description
 
-Pix is an imaginary “Over The Top” (OTT) platform. It is an app built as a console-based application using Java, JDBC and MySQL database. It has the layout consists of Login, Sign up and Exit options.
+Pix is an imaginary “Over The Top” (ott) platform. It is an app built as a console-based application using Java, JDBC and MySQL database. It has the layout consists of Login, Sign up and Exit options.
 For login, user input will be collected from console and JDBC connection will be established to fetch the user details and allows user to login.
 If user details are valid, layout options will consist of Top 5 Movies, Show History and WishList. Each options entered will establish JDBC connection and displays the fetched details from DAO.
 For signup, user's mobile number, user name and password will be collected and store in a USER table.
@@ -22,21 +22,26 @@ If exit option is chosen, it would stop the application.
 
 ## Features
 
-* User can create an account using Pix-OTT platoform using their mobile no, user name and password.
-* Favourite movies can be added in a wishlist by user.
-* Has admin controller which gives access to update the movie details.
+* User can create an account using Pix-ott platoform using their mobile no, user name and password.
+* User can login that account.
+* User can view the TOP 5 Movies currently trending movies.
+* User can search movies based on keywords. 
 
 To-do list:
 * User input field validation to be taken place when login/sign up.
 * To add unavailable movies to the movie list.
 * To delete movie history and movie wishlist.
+* To add a movie to Wish List.
+* To play a movie from top 5, to watch a movie.
+* Admin I want remove and make another movie for top 5.
+* Admin I want to update movies details. 
 
 ## Getting Started
 
-  Download source code from https://github.com/AlwinAlphabot/Pix-OTT
+  Download source code from https://github.com/Mohanraj3521/Pix-ott
   - Import the project in Eclipse/Springtoolsuite IDE.
   - Add a driver manager in library.
-  - Create an user table, movie table, topfivemovie table, history table and wishlist table with columns mentioned in DAO files in pixott schema.
+  - Create an user table, movie table, topfivemovie table mentioned in DAO files in pixott schema.
   - Point the url to localhost in app.properties and provide DB credentials.
 
 ## Usage
